4595580ef721cde1bde7d27bc413fa668f2752e4
Aqib0302 <118673263+Aqib0302@users.noreply.github.com>
2025-10-05 22:15:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+let el  = document.querySelector("p");
+
+setInterval(()=>{
+	let d=new Date();
+let seconds=d.getSeconds();
+let hour=d.getHours();
+let minutes=d.getMinutes();
+let date=d.getDate();
+let month=d.getMonth()+1;
+let year=d.getFullYear();
+ let AM2PM="";
+	if(hour>=12){
+		AM2PM="PM";
+	}
+	else{
+		AM2PM="AM";
+	}
+	el.innerHTML=`<h2> ${date}/${month}/${year}</h2>,${hour}:${minutes}:${seconds} ${AM2PM}`;
+},1000);
